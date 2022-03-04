16b811212c6e92fa9e7015636604c626e8fbb9ba
Jesus-7777 <61198234+Jesus-7777@users.noreply.github.com>
2022-03-04 11:57:06 -0500

    segunda actualizacion

--- app.py
+++ app.py
@@ -6,4 +6,19 @@ jinja2 motor de plantillas
 (pip freeze) sirbe para ve que plugins necesita 
 pip freeze > requirements.txt
 pip install -r requirements.txt
+
+
+clase 3 
+
+actividad de GRUD
+
+listar 
+crear
+Productos 
+Id              int(11)             aut.      Pk    
+nombre          varcahr(255)                  Not Null
+precio          decimal(15.2)                 Not Null  
+
+
+
 """
--- main.py
+++ main.py
@@ -1,16 +1,66 @@
-from flask import Flask, render_template, template_rendered
+from fileinput import close
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
 
+#TODO: conectar a la base de datos
+
+db = mysql.connector.connect(
+host ='localhost',
+password='', 
+user='root',
+database="pro_crud",
+port=3306
+
+)
+db.autocommit=True #TODO: esta linea es para que la base de datos no guarde la cahe
 """Instancia de la variable name"""
+
 app = Flask(__name__) 
 
 """Funcion decoradora(herencia)"""
 @app.get("/")
 def index():
-    return render_template("index.html")
+    #TODO:ponemos abrimos una variable cursor para conectar a la base de datos
+   #el dictionario se pone para que la informacion sea legible y poder entenderla, se pone solo para consultar datos
+    
+    cursor=db.cursor(dictionary=True)
+    
+    #TODO:esto resive solo una cadena de texto, se manda cualquier consulta SQL
+    cursor.execute('select * from productos')
+    
+    productos= cursor.fetchall()
+    #TODO:productos= cursor.fetchone()-nos muestra un producto
+    
+    #TODO:print(productos[1]['nombre']) 
+    
+    #TODO:luego cerramos el cursor para que no se quede abirta la base de datos
+    cursor= close()
+    return render_template("index.html",productos=productos)
 
+
+#TODO: fromulario para crear producto
+@app.get("/crear")
+def crearProducto():
+    return render_template("crear.html")
+
+
+#TODO: metodo para enviar el formulario a la base de datos
+@app.post("/crear")
+def crearProductoPost():
+    nombre=request.form.get('nombre')
+    precio=request.form.get('precio')
+    cursor=db.cursor()
+    cursor.execute("insert into productos(nombre, precio) values(%s,%s)",(nombre,precio,))
+    cursor.close()
+    
+    return redirect(url_for('index'))   
+
+ #TODO: pagina de contactos
 @app.get("/contacto")
 def contacto():
     return render_template("/contactos/index.html")
+
+#TODO: pagina para editar el id de l contacto
 @app.get("/contacto/<contactoId>/edit")
 def editarContacto(contactoId):
     
@@ -19,6 +69,7 @@ def editarContacto(contactoId):
     
     #http://localhost:5000/contacto/6/edit
 
+#TODO: calcular año de nacimiento
 @app.get("/contacto/<int:nacimientoId>/edad/")
 def ediEdad(nacimientoId):
     
